3f954a6f08ebbc5e891cf34b053304de3b983871
Deuri-mojica <84188101+Deuri-mojica@users.noreply.github.com>
2021-06-22 11:47:17 -0400

    Update app.js

--- src/src/app.js
+++ src/src/app.js
@@ -4,13 +4,12 @@ var ancho = document.getElementById("ancho");
 var alto = document.getElementById("alto");
 var mensaje = document.getElementById("mensaje");
 
-
+//funcion que al ejecutarse modifica los volores del contenedor
 function modificar() {
     var contendor = document.getElementById("container");
     contendor.style.width = ancho.value;
     contendor.style.height = alto.value;
     contendor.innerHTML = mensaje.value;
-
 }
-
+// aqui escuchmos el evento clic en el botom y ejecutamos la funcion modificar
 const button = document.getElementById("btn-btn").addEventListener("click", modificar);
